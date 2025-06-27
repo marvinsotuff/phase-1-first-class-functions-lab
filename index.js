a180ba43cc69b3bc91da7a431e43f8ae597828c2
Marvin Kioko <kiokomarvin7@gmail.com>
2025-06-27 10:10:27 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+function returnFirstTwoDrivers(drivers) {
+  return [drivers[0], drivers[1]];
+}
+
+function returnLastTwoDrivers(drivers) {
+    return [drivers[2], drivers[3]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function fareDoubler(fare){
+        return fare * multiplier;
+    }
+}
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+}
